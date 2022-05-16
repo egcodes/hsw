ab723baf7b8a17582f6110072978b69ab26e6630
erdi.gurbuz <erdigurbuz@gmail.com>
2022-05-16 22:03:09 +0300

    create Connection, Activity entities

---
+++ src/main/java/com/hackerswork/hsw/persistence/entity/Activity.java
@@ -0,0 +1,34 @@
+package com.hackerswork.hsw.persistence.entity;
+
+import java.io.Serializable;
+import java.time.OffsetDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+public class Activity implements Serializable {
+
+    private static final long serialVersionUID = -2241699815447690916L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private Long personId;
+
+    @NotNull
+    private OffsetDateTime lastActivityTime;
+
+}
---
+++ src/main/java/com/hackerswork/hsw/persistence/entity/Connection.java
@@ -0,0 +1,36 @@
+package com.hackerswork.hsw.persistence.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+public class Connection implements Serializable {
+
+    private static final long serialVersionUID = 1858746340416571229L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private Long personId;
+
+    @NotNull
+    private Long connectionId;
+
+    private boolean hidden;
+
+    private boolean pinned;
+}
