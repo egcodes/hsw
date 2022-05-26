ec5676ea601fbe819683e5e52428773687fbc931
erdi.gurbuz <erdigurbuz@gmail.com>
2022-05-26 20:07:59 +0300

    change status name & refactor

--- src/main/java/com/hackerswork/hsw/enums/Status.java
+++ src/main/java/com/hackerswork/hsw/enums/Status.java
@@ -1,14 +1,8 @@
 package com.hackerswork.hsw.enums;
 
 public enum Status {
-    WAITING_FOR_APPROVAL("Waiting For Approval"),
-    ACTIVE("Active"),
-    DEACTIVATE("Deactive");
-
-    private String displayName;
-
-    Status(String displayName) {
-        this.displayName = displayName;
-    }
+    PASSIVE,
+    ACTIVE,
+    WAITING_FOR_APPROVAL
 
 }
