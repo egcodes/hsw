2d4dde9201c248ffde97e158272190a049f3b252
erdi.gurbuz <erdigurbuz@gmail.com>
2022-05-27 21:32:38 +0300

    remove unused method

--- src/main/java/com/hackerswork/hsw/api/controller/PersonController.java
+++ src/main/java/com/hackerswork/hsw/api/controller/PersonController.java
@@ -6,7 +6,6 @@ import com.hackerswork.hsw.service.person.PersonCommandService;
 import com.hackerswork.hsw.service.person.PersonQueryService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-
 
 @RestController
 @RequestMapping("/person")
@@ -36,13 +34,7 @@ public class PersonController {
     @GetMapping(value = "/{id}")
     @ApiOperation(value = "Get person", notes = "Get person by id")
     public ResponseEntity<PersonDTO> get(@PathVariable Long id) {
-        return ResponseEntity.ok(personMapper.toDTO(personQueryService.getPerson(id)));
-    }
-
-    @GetMapping(value = "/list")
-    @ApiOperation(value = "Get persons", notes = "List all persons")
-    public ResponseEntity<List<PersonDTO>> list() {
-        return ResponseEntity.ok(personMapper.toDTOs(personQueryService.getPersons()));
+        return ResponseEntity.ok(personMapper.toDTO(personQueryService.findPerson(id).get()));
     }
 
 }
