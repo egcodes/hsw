5b957482a3cf9aebbe60449904ad12208b4953e8
erdi.gurbuz <erdigurbuz@gmail.com>
2022-05-28 20:54:43 +0300

    add query to find person activity

--- src/main/java/com/hackerswork/hsw/persistence/repository/ActivityRepository.java
+++ src/main/java/com/hackerswork/hsw/persistence/repository/ActivityRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
 
+    Activity findByPersonId(Long personId);
 }
