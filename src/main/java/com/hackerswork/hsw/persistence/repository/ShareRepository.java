2f6903cbc0a80280568367a871389153c0f8804b
erdi.gurbuz <erdigurbuz@gmail.com>
2022-06-07 21:56:57 +0300

    add query to fetch complex post data

--- src/main/java/com/hackerswork/hsw/persistence/repository/ShareRepository.java
+++ src/main/java/com/hackerswork/hsw/persistence/repository/ShareRepository.java
@@ -1,13 +1,19 @@
 package com.hackerswork.hsw.persistence.repository;
 
+import com.hackerswork.hsw.dto.ShareDTO;
 import com.hackerswork.hsw.persistence.entity.Share;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ShareRepository extends JpaRepository<Share, Long> {
 
-    List<Share> findByPersonId(Long personId);
-
+    @Query("SELECT new com.hackerswork.hsw.dto.ShareDTO(p.userName, p.name, s.text, s.createdTime) FROM Person p LEFT JOIN Share s "
+        + "ON p.id = s.personId "
+        + "WHERE s.personId IN :personIds "
+        + "ORDER BY s.createdTime DESC")
+    List<ShareDTO> findAllByPersonIds(List<Long> personIds, Pageable pageable);
 }
