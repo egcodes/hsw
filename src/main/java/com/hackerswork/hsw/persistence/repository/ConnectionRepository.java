f31c6a5182e2cd3125d0c843a20d5a3278f68489
erdi.gurbuz <erdigurbuz@gmail.com>
2022-06-07 21:59:42 +0300

    use query to fetch only connectionId

--- src/main/java/com/hackerswork/hsw/persistence/repository/ConnectionRepository.java
+++ src/main/java/com/hackerswork/hsw/persistence/repository/ConnectionRepository.java
@@ -3,10 +3,12 @@ package com.hackerswork.hsw.persistence.repository;
 import com.hackerswork.hsw.persistence.entity.Connection;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ConnectionRepository extends JpaRepository<Connection, Long> {
 
-    List<Connection> findByPersonId(Long personId);
+    @Query("SELECT c.connectionId FROM Connection c WHERE c.personId = :personId")
+    List<Long> findConnectionsByPersonId(Long personId);
 }
