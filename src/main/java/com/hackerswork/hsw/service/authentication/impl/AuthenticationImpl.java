b4f8f7023cae432dc1a1c02509ce9829d3ef2c07
erdi.gurbuz <erdigurbuz@gmail.com>
2022-06-08 21:31:54 +0300

    create person if isEmpty or partial

--- src/main/java/com/hackerswork/hsw/service/authentication/impl/AuthenticationImpl.java
+++ src/main/java/com/hackerswork/hsw/service/authentication/impl/AuthenticationImpl.java
@@ -1,14 +1,15 @@
 package com.hackerswork.hsw.service.authentication.impl;
 
 import com.hackerswork.hsw.enums.Auth;
+import com.hackerswork.hsw.enums.Status;
 import com.hackerswork.hsw.persistence.entity.Person;
 import com.hackerswork.hsw.service.authentication.AuthProvider;
 import com.hackerswork.hsw.service.authentication.Authentication;
 import com.hackerswork.hsw.service.person.PersonCommandService;
+import com.hackerswork.hsw.service.person.PersonQueryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,6 +19,7 @@ public class AuthenticationImpl implements Authentication {
 
     private final AuthProvider authProvider;
     private final PersonCommandService personCommandService;
+    private final PersonQueryService personQueryService;
 
     @Override
     public Person login(Auth auth, String code) {
@@ -26,11 +28,20 @@ public class AuthenticationImpl implements Authentication {
                 var userPossible = authProvider.login(code);
                 if (userPossible.isPresent()) {
                     var user = userPossible.get();
-                    return personCommandService.add(Person.builder()
-                        .name(user.getName())
-                        .userName(user.getLogin())
-                        .mail(user.getEmail())
-                        .build());
+
+                    var personPossible = personQueryService.findPersonByUserName(user.getLogin());
+                    if (personPossible.isEmpty() || Status.PARTIAL.equals(personPossible.get().getStatus())) {
+                        var newPerson = personCommandService.add(Person.builder()
+                            .name(user.getName())
+                            .userName(user.getLogin())
+                            .mail(user.getEmail())
+                            .status(Status.ACTIVE)
+                            .build());
+                        newPerson.setStatus(Status.NEW);
+                        return newPerson;
+                    }
+
+                    return personPossible.get();
                 }
                 break;
             case LINKEDIN:
