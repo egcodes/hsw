bd8faaf32326a3c5414574590a9cbf65a71049ba
erdi.gurbuz <erdigurbuz@gmail.com>
2022-06-08 21:35:15 +0300

    add setter methods with lombok

--- src/main/java/com/hackerswork/hsw/dto/UserDTO.java
+++ src/main/java/com/hackerswork/hsw/dto/UserDTO.java
@@ -2,9 +2,11 @@ package com.hackerswork.hsw.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Builder
 @Getter
+@Setter
 public class UserDTO {
 
     private String login;
