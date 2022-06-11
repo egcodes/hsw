31843fddacc7156f830d5328dd7013c8d603e7eb
erdi.gurbuz <erdigurbuz@gmail.com>
2022-06-11 23:28:19 +0300

    refactor finding activity for person

--- src/main/java/com/hackerswork/hsw/enums/Activity.java
+++ src/main/java/com/hackerswork/hsw/enums/Activity.java
@@ -1,7 +1,16 @@
 package com.hackerswork.hsw.enums;
 
+import static com.hackerswork.hsw.constants.Constant.DURATION_FOR_ONLINE;
+
+import java.time.Instant;
+
 public enum Activity {
-    OFFLINE,
-    BUSY,
-    ONLINE
+    OFFLINE, BUSY, ONLINE;
+
+    public static Activity findBy(Long lastActivityTime) {
+        var checkTime = Instant.now().minusMillis(DURATION_FOR_ONLINE).toEpochMilli();
+        if (lastActivityTime > checkTime)
+            return ONLINE;
+        return OFFLINE;
+    }
 }
--- src/main/java/com/hackerswork/hsw/service/ConnectionActivityServiceImpl.java
+++ src/main/java/com/hackerswork/hsw/service/ConnectionActivityServiceImpl.java
@@ -1,12 +1,9 @@
 package com.hackerswork.hsw.service;
 
-import static com.hackerswork.hsw.constants.Constant.DURATION_FOR_ONLINE;
-
 import com.hackerswork.hsw.dto.ConnectionActivityDTO;
 import com.hackerswork.hsw.enums.Activity;
 import com.hackerswork.hsw.service.activity.ActivityQueryService;
 import com.hackerswork.hsw.service.connection.ConnectionQueryService;
-import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -37,9 +34,6 @@ public class ConnectionActivityServiceImpl implements ConnectionActivityService 
     }
 
     private Activity getActivity(Long lastActivityTime) {
-        var checkTime = Instant.now().minusMillis(DURATION_FOR_ONLINE).toEpochMilli();
-        if (lastActivityTime > checkTime)
-            return Activity.ONLINE;
-        return Activity.OFFLINE;
+        return Activity.findBy(lastActivityTime);
     }
 }
