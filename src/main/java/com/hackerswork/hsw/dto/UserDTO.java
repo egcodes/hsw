a97beba8cf782c5eb2581e04132f6267a5c65f45
erdi.gurbuz <erdigurbuz@gmail.com>
2022-07-04 11:48:53 +0300

    add toString

--- src/main/java/com/hackerswork/hsw/dto/UserDTO.java
+++ src/main/java/com/hackerswork/hsw/dto/UserDTO.java
@@ -3,10 +3,12 @@ package com.hackerswork.hsw.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Builder
 @Getter
 @Setter
+@ToString
 public class UserDTO {
 
     private String login;
