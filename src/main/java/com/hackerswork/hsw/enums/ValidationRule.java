e021356bdf4ee6c734c226f9dd41e29e0f66b5c8
erdi.gurbuz <erdigurbuz@gmail.com>
2022-07-05 20:33:32 +0300

    add new message

--- src/main/java/com/hackerswork/hsw/enums/ValidationRule.java
+++ src/main/java/com/hackerswork/hsw/enums/ValidationRule.java
@@ -7,7 +7,8 @@ public enum ValidationRule {
 
     PERSON_NOT_FOUND("Person not found"),
     SHARE_NOT_FOUND("Share not found"),
-    COULD_NOT_SIGN_IN("Could not sign in");
+    COULD_NOT_SIGN_IN("Could not sign in"),
+    INVALID_TOKEN("Invalid token");
 
     private final String error;
 
