9271f4b89f99f6c4c4067d787c8332e767976bed
erdi.gurbuz <erdigurbuz@gmail.com>
2022-07-06 20:56:30 +0300

    ignore from search result already connected

--- src/main/java/com/hackerswork/hsw/api/controller/PersonController.java
+++ src/main/java/com/hackerswork/hsw/api/controller/PersonController.java
@@ -3,12 +3,14 @@ package com.hackerswork.hsw.api.controller;
 import com.hackerswork.hsw.dto.PersonDTO;
 import com.hackerswork.hsw.enums.Status;
 import com.hackerswork.hsw.mapper.PersonMapper;
+import com.hackerswork.hsw.service.connection.ConnectionQueryService;
 import com.hackerswork.hsw.service.person.PersonCommandService;
 import com.hackerswork.hsw.service.person.PersonQueryService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +28,7 @@ public class PersonController {
 
     private final PersonCommandService personCommandService;
     private final PersonQueryService personQueryService;
+    private final ConnectionQueryService connectionQueryService;
     private final PersonMapper personMapper;
 
     @PostMapping(value = "/add")
@@ -42,10 +45,14 @@ public class PersonController {
 
     @GetMapping(value = "/search/keyword={text}")
     @ApiOperation(value = "Find persons", notes = "Find person by keyword")
-    public ResponseEntity<List<PersonDTO>> get(@PathVariable String text) {
+    public ResponseEntity<List<PersonDTO>> get(@RequestHeader("personId") Long personId, @PathVariable String text) {
         var foundPersons = new ArrayList<PersonDTO>();
         foundPersons.addAll(personMapper.toDTOs(personQueryService.findByUserNameLike(Status.ACTIVE, text)));
         foundPersons.addAll(personMapper.toDTOs(personQueryService.findByNameLike(Status.ACTIVE, text)));
-        return ResponseEntity.ok(foundPersons);
+
+        var personConnections = connectionQueryService.findConnectionIds(personId);
+        return ResponseEntity.ok(foundPersons.stream()
+            .filter(p -> !personConnections.contains(p.getId()))
+            .collect(Collectors.toList()));
     }
 }
