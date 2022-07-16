9740663cd68915ba5956205bdbc825e9701d9129
erdi.gurbuz <erdigurbuz@gmail.com>
2022-07-16 11:36:51 +0300

    add cookie method

--- src/main/java/com/hackerswork/hsw/api/controller/AuthenticationController.java
+++ src/main/java/com/hackerswork/hsw/api/controller/AuthenticationController.java
@@ -2,6 +2,7 @@ package com.hackerswork.hsw.api.controller;
 
 import static com.hackerswork.hsw.constants.Constant.AUTHENTICATION_PATH;
 
+import com.hackerswork.hsw.constants.Constant;
 import com.hackerswork.hsw.dto.PersonDTO;
 import com.hackerswork.hsw.enums.Auth;
 import com.hackerswork.hsw.mapper.PersonMapper;
@@ -11,6 +12,8 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,4 +36,20 @@ public class AuthenticationController {
         return ResponseEntity.ok(personMapper.toDTO(authentication.login(auth, code)));
     }
 
+    @GetMapping(value = "createCookie/{token}")
+    @ApiOperation(value = "Create a cookie", notes = "")
+    public ResponseEntity<?> createCookie(@PathVariable String token) {
+        var cookie = ResponseCookie.from(Constant.COOKIE_NAME, token)
+            .httpOnly(true)
+            .secure(true)
+            .sameSite("Lax")
+            .path("/")
+            .maxAge(Constant.COOKIE_EXPIRE_TIME)
+            .build();
+
+        return ResponseEntity
+            .ok()
+            .header(HttpHeaders.SET_COOKIE, cookie.toString())
+            .build();
+    }
 }
