5191e53372756f6430009fa423e1410a983ba9de
erdi.gurbuz <erdigurbuz@gmail.com>
2022-07-16 11:38:12 +0300

    add personId, expireDate - rename token column

--- src/main/java/com/hackerswork/hsw/persistence/entity/Token.java
+++ src/main/java/com/hackerswork/hsw/persistence/entity/Token.java
@@ -5,12 +5,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import org.hibernate.validator.constraints.Length;
 
 @Entity
@@ -18,6 +21,10 @@ import org.hibernate.validator.constraints.Length;
 @AllArgsConstructor
 @RequiredArgsConstructor
 @Getter
+@ToString
+@Table(indexes = {
+    @Index(name="token_token", columnList="token")
+})
 public class Token implements Serializable {
 
     private static final long serialVersionUID = 3018776368057112848L;
@@ -29,9 +36,14 @@ public class Token implements Serializable {
     @NotNull
     private String userName;
 
+    @NotNull
+    private Long personId;
+
     @Setter
     @NotNull
     @Length(max = 255)
-    private String text;
+    private String token;
 
+    @Setter
+    private Long expireDate;
 }
