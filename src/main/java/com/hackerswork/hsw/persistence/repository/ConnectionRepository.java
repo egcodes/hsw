b9f64c5187ef9b837d6a1a3259687d81bc40daac
erdi.gurbuz <erdigurbuz@gmail.com>
2022-07-23 16:11:11 +0300

    update

--- src/main/java/com/hackerswork/hsw/persistence/repository/ConnectionRepository.java
+++ src/main/java/com/hackerswork/hsw/persistence/repository/ConnectionRepository.java
@@ -19,17 +19,17 @@ public interface ConnectionRepository extends JpaRepository<Connection, Long> {
     List<ConnectionDTO> findAllByPersonId(Long personId);
 
     @Query("SELECT c.connectionId FROM Connection c LEFT JOIN Person p ON c.connectionId = p.id"
-        + " WHERE p.status = :status AND c.personId = :personId")
+        + " WHERE p.status = :status AND c.blocked = false AND c.hidden = false AND c.personId = :personId")
     List<Long> findConnectionsByPersonId(Long personId, Status status);
 
     int countByConnectionId(Long connectionId);
 
     @Query("SELECT new com.hackerswork.hsw.dto.PersonSumDTO(p.id, p.userName, p.name)  FROM Connection c LEFT JOIN Person p ON c.personId = p.id"
-        + " WHERE p.status = :status AND c.connectionId = :personId")
+        + " WHERE p.status = :status AND c.blocked = false AND c.connectionId = :personId")
     List<PersonSumDTO> findNameByConnectionId(Long personId, Status status);
 
     @Query("SELECT new com.hackerswork.hsw.dto.PersonSumDTO(p.id, p.userName, p.name) FROM Connection c LEFT JOIN Person p ON c.connectionId = p.id"
-        + " WHERE p.status = :status AND c.personId = :personId")
+        + " WHERE p.status = :status AND c.blocked = false AND c.personId = :personId")
     List<PersonSumDTO> findNameByPersonId(Long personId, Status status);
 
     @Query("SELECT new com.hackerswork.hsw.dto.PersonSumDTO(p.id, p.userName, p.name) FROM Connection c LEFT JOIN Person p ON c.connectionId = p.id"
