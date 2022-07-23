73b8505cccdad0a7f09872d3c14810835da69e15
erdi.gurbuz <erdigurbuz@gmail.com>
2022-07-23 12:50:23 +0300

    add new status

--- src/main/java/com/hackerswork/hsw/enums/Activity.java
+++ src/main/java/com/hackerswork/hsw/enums/Activity.java
@@ -5,7 +5,7 @@ import static com.hackerswork.hsw.constants.Constant.DURATION_FOR_ONLINE;
 import java.time.Instant;
 
 public enum Activity {
-    OFFLINE, BUSY, ONLINE;
+    OFFLINE, BUSY, ONLINE, AWAY;
 
     public static Activity findBy(Long lastActivityTime) {
         var checkTime = Instant.now().minusMillis(DURATION_FOR_ONLINE).toEpochMilli();
